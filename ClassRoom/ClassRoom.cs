10a78d58f869a9a0639a37cc796b7bfd87bdd5ff
Joshua James Lewis <josh1205_1933@yahoo.com>
2020-08-26 13:02:46 +0200

    Task 2 - Added Classroom Class

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class ClassRoom
+    {
+        private string _className;
+        private DateTime _semesterStart;
+
+        List<string> classList = new List<string>();
+
+        public string ClassName { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+            
+        }
+        
+
+
+
+
+
+    }
+}

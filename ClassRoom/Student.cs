fbadacb68c6f0d2b4a9f64f2cc13422f26724e3c
Joshua James Lewis <josh1205_1933@yahoo.com>
2020-08-26 12:41:41 +0200

    Task 1 - Student Class Creation

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthday;
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            _name = name;
+            _birthMonth = birthMonth;
+            _birthday = birthday;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int BirthMonth
+        {
+            get { return _birthMonth; }
+            set { _birthMonth = value; }
+        }
+
+        public int Birthday
+        {
+            get { return _birthday; }
+            set { _birthday = value; }
+        }
+
+    }
+}
